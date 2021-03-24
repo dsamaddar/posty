c6c21defd9a8b4e41b9e25687f840a278ab92c5e
dsamaddar <debumist@gmail.com>
2021-03-24 18:12:19 +0600

    commit-01

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    public function index(){
+        return view('auth.register');
+    }
+
+    public function store(Request $request){
+        //dd($request);
+        //dd($request->email);
+
+        //validation
+        $this->validate($request,[
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email',
+            'password' => 'required|confirmed'
+        ]);
+
+        //Store Data
+        User::Create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        //sign in
+        Auth::attempt($request->only('email','password'));
+
+        //redirect user
+        return redirect()->route('dashboard');
+
+    }
+}
